cac2d83ba6051205edec5fd040384b1d91c05229
Krzysztof Radomski <krzysztofradomski@krs.local>
2019-02-07 16:01:19 +0100

    WIP

--- client/client.js
+++ client/client.js
@@ -1,9 +1,23 @@
-var socket = io();
+var socket = io()
 
-socket.on('connectToRoom',function(data) {
-   document.getElementById('room').textContent = data;
-});
-
-socket.on('roomsById', function(data){
-   console.log('roomsById', data)
+socket.on('connectToRoom', function(data) {
+  document.getElementById('room').textContent = data
 })
+
+socket.on('roomsData', function(data) {
+  console.log('roomsData', data)
+  createStructure(data)
+})
+
+function createStructure(data) {
+  let container = document.getElementById('rooms')
+  container.innerHTML = ''
+  container.setAttribute('class', 'container')
+  document.body.appendChild(container)
+  for (let i = 0; i < data.amount; i++) {
+    let div = document.createElement('div')
+    div.setAttribute('class', 'tile')
+    div.innerText = data.rooms[i].id
+    container.appendChild(div)
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,47 +1,59 @@
 var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-var config = require('./config');
-
+var config = require('./config')
 
 function startServer() {
+  var roomNumber = 1
+  var roomsById = []
+  var allClients = []
 
-  var roomNumber = 1;
-  var roomsById = [];
-
-  app.use(express.static('client'));
+  app.use(express.static('client'))
   io.on('connection', function(socket) {
-    var currentRoomNumber = 'room-' + roomNumber;
-    // Increase roomNumber if 2 clients are present in a room.
+    allClients.push(socket)
+    var currentRoomNumber = 'room-' + roomNumber
+    // Increase roomNumber if MAX_ROOM_SIZE clients are present in a room.
     if (
       io.nsps['/'].adapter.rooms[currentRoomNumber] &&
-      io.nsps['/'].adapter.rooms[currentRoomNumber].length >= config.MAX_ROOM_SIZE
+      io.nsps['/'].adapter.rooms[currentRoomNumber].length >
+        config.MAX_ROOM_SIZE - 1
     ) {
-      roomNumber++;
+      roomNumber++
+      currentRoomNumber = 'room-' + roomNumber
     }
     // Update rooms' stats.
     if (roomsById.indexOf(currentRoomNumber) < 0) {
-      roomsById.push(currentRoomNumber);
+      roomsById.push(currentRoomNumber)
     }
 
-    socket.join(currentRoomNumber);
-  
+    socket.join(currentRoomNumber)
+    socket.on('disconnect', function() {
+      console.log('Got disconnect!')
+      var i = allClients.indexOf(socket)
+      allClients.splice(i, 1)
+      socket.leave(currentRoomNumber)
+    })
+
     // Send this event to everyone in the room.
-    io.sockets
-      .in(currentRoomNumber)
-      .emit('connectToRoom', 'You are in room no. ' + roomNumber);
+    io.sockets.in(currentRoomNumber).emit('connectToRoom', roomNumber)
 
     // Broadcast stats to everyone connected.
-    io.emit('roomsById', roomsById);
-  });
-  
-  http.listen(3000, function() {
-    console.log('Listening on localhost:3000');
-  });
+    var roomsData = {
+      amount: roomsById.length,
+      roomsById: roomsById,
+      rooms: roomsById.map(room => ({
+        id: room,
+        data: io.nsps['/'].adapter.rooms[room]
+      }))
+    }
+    io.emit('roomsData', roomsData)
+  })
 
+  http.listen(3000, function() {
+    console.log('Listening on localhost:3000')
+  })
 }
 
-module.exports = startServer;
-
+module.exports = startServer
